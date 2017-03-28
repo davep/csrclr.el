8a58359dded08cbe2020b3a0b664b853ce21fc0c
Dave Pearson <davep@davep.org>
2017-03-28 14:44:10 +0100

    Keep checkdoc happy

--- csrclr.el
+++ csrclr.el
@@ -6,7 +6,13 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/csrclr.el
 
-;; Customize options.
+;;; Commentary:
+;;
+;; csrclr.el provides a command that sets the cursor to a different colour
+;; depending on the environment. I find this especially handy if I open a
+;; read-only buffer.
+
+;;; Code:
 
 (defgroup csrclr nil
   "Cursor colour."
